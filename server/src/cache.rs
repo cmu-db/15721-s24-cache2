6b1fa98f62a1b65f24ed5037cb2a07a0ee26cb2c
J-HowHuang <howardhuang2001@gmail.com>
2024-02-19 12:54:01 -0500

    add redis connection

--- server/src/cache.rs
+++ server/src/cache.rs
@@ -1,8 +1,13 @@
 use rocket::fs::NamedFile;
 use std::path::{Path, PathBuf};
+use log::{info};
 
 pub struct FileUid {
     file_name: PathBuf
+}
+
+pub struct CacheServer{
+    conn: redis::Connection
 }
 
 impl FileUid {
@@ -13,11 +18,20 @@ impl FileUid {
     }
 }
 
-pub async fn request_file(uid: FileUid) -> Option<NamedFile> {
-    /* [TODO] check if <uid> file exist on local disk */
+impl CacheServer{
+    pub fn new(addr: &str) -> Result<Self, redis::RedisError> {
+        let client = redis::Client::open(addr)?;
+        let conn = client.get_connection()?;
+        info!("Connected with server {}", addr);
+        Ok(CacheServer {
+            conn
+        })
+    }
+    pub async fn request_file(&self, uid: FileUid) -> Option<NamedFile> {
+            /* [DEBUG USE] "data/" is tmp dummy directory */
+            NamedFile::open(Path::new("debug_data/").join(uid.file_name)).await.ok()
+        } 
+        /* [TODO] check if <uid> file exist on local disk */
 
-    /* [TODO] if <uid> doesn't exist, fetch from cloud storage */
-
-    /* [DEBUG USE] "data/" is tmp dummy directory */
-    NamedFile::open(Path::new("debug_data/").join(uid.file_name)).await.ok()
+        /* [TODO] if <uid> doesn't exist, fetch from cloud storage */
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -6,7 +6,7 @@ mod db;
 
 use std::path::PathBuf;
 use rocket::fs::NamedFile;
-use crate::cache::*;
+use rocket::State;
 
 #[get("/")]
 fn health_check() -> &'static str {
@@ -14,14 +14,16 @@ fn health_check() -> &'static str {
 }
 
 #[get("/s3/<path..>")]
-async fn get_file(path: PathBuf) -> Option<NamedFile> {
-    let uid = FileUid::from_path(path);
-    request_file(uid).await
+async fn get_file(path: PathBuf, cache_server: &State<cache::CacheServer>) -> Option<NamedFile> {
+    let uid = cache::FileUid::from_path(path);
+    cache_server.request_file(uid).await
 }
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
+    let cache_server = cache::CacheServer::new("redis://127.0.0.1:6739").unwrap();
     let _rocket = rocket::build()
+        .manage(cache_server)
         .mount("/", routes![health_check, get_file])
         .launch()
         .await?;
