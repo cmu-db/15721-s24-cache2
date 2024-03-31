6323c6c3e6e7df7fb7450b02dbb3c77f6bf14fca
J-HowHuang <howardhuang2001@gmail.com>
2024-03-31 12:43:54 -0400

    add rust integration test

---
+++ server/tests/integration.rs
@@ -0,0 +1,82 @@
+use rocket::http::Status;
+
+mod utils;
+
+#[test]
+fn test_healthy() {
+    let (_, [client_1, client_2, client_3]) 
+        = utils::launch_server_node_size_3();
+
+    let response = client_1.get("/").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.into_string(), Some("Healthy\n".into()));
+
+    let response = client_2.get("/").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.into_string(), Some("Healthy\n".into()));
+
+    let response = client_3.get("/").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.into_string(), Some("Healthy\n".into()));
+}
+
+#[test]
+fn test_clear() {
+    let (_, [client_1, _, _]) = utils::launch_server_node_size_3();
+    
+    let _ = client_1.get("/s3/test2.txt").dispatch();
+    let response = client_1.get("/stats").dispatch();
+    let stats = response.into_string().unwrap();
+    assert!(stats.contains("test2"));
+
+    
+    let response = client_1.post("/clear").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    let response = client_1.get("/stats").dispatch();
+    let stats = response.into_string().unwrap();
+    assert!(!stats.contains("test2"));
+
+}
+
+#[test]
+fn test_get_file() {
+    let (_, [client_1, client_2, client_3]) = utils::launch_server_node_size_3();
+    let response = client_1.get("/s3/test1.txt").dispatch();
+    assert_eq!(response.status(), Status::SeeOther);
+    let response = client_1.get("/s3/test2.txt").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    let response = client_2.get("/s3/test3.txt").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    let response = client_2.get("/s3/test4.txt").dispatch();
+    assert_eq!(response.status(), Status::SeeOther);
+    let response = client_3.get("/s3/test5.txt").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    let response = client_3.get("/s3/test6.txt").dispatch();
+    assert_eq!(response.status(), Status::SeeOther);
+
+    
+    let response = client_1.post("/clear").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    let response = client_2.post("/clear").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    let response = client_3.post("/clear").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+}
+
+#[test]
+fn test_evict() {
+    let (_, [client_1, _, _]) = utils::launch_server_node_size_3();
+
+    let response = client_1.post("/clear").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    let response = client_1.get("/s3/test6.txt").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    let response = client_1.get("/s3/test8.txt").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    let response = client_1.get("/s3/test12.txt").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    let response = client_1.get("/stats").dispatch();
+    let stats = response.into_string().unwrap();
+    assert!(!stats.contains("test6"));
+    assert!(stats.contains("test8"));
+}
---
+++ server/tests/utils.rs
@@ -0,0 +1,29 @@
+use istziio_server_node::server::{ServerConfig, ServerNode};
+use rocket::local::blocking::Client;
+
+pub fn get_server_config_mocks3(redis_port: u16) -> ServerConfig {
+    ServerConfig {
+        redis_port,
+        cache_dir: format!("./cache_{}", redis_port),
+        use_mock_s3_endpoint: Some(String::from("http://0.0.0.0:6333")),
+        bucket: None,
+        region_name: None,
+        access_key: None,
+        secret_key: None
+    }
+}
+
+pub fn launch_server_node_size_3() -> ([ServerNode; 3], [Client; 3]) {
+    let config_1 = get_server_config_mocks3(6379);
+    let config_2 = get_server_config_mocks3(6380);
+    let config_3 = get_server_config_mocks3(6381);
+    
+    let node_1 = ServerNode::new(config_1);
+    let node_2 = ServerNode::new(config_2);
+    let node_3 = ServerNode::new(config_3);
+
+    let client_1 = Client::tracked(node_1.build()).expect("valid rocket instance");
+    let client_2 = Client::tracked(node_2.build()).expect("valid rocket instance");
+    let client_3 = Client::tracked(node_3.build()).expect("valid rocket instance");
+    ([node_1, node_2, node_3], [client_1, client_2, client_3])
+}
